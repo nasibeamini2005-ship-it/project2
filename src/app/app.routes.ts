7f575e7b34aa975d6098a1e8cb9afc049cdc6abd
Nasibeh Amini <nasibe.amini2005@gmail.com>
2025-11-22 05:46:44 +0330

    Routes Creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { PublicTemplate } from './+pages/+public/public-template/public-template';
+import { HomePage } from './+pages/+public/home-page/home-page';
+import { AboutPage } from './+pages/+public/about-page/about-page';
+import { ContactPage } from './+pages/+public/contact-page/contact-page';
+import { PrivateTemplate } from './+pages/+private/private-template/private-template';
+import { DashboardPage } from './+pages/+private/dashboard-page/dashboard-page';
+import { UsersPage } from './+pages/+private/users-page/users-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'public', component:PublicTemplate,children:[
+        {path:'home', component:HomePage},
+        {path:'about', component:AboutPage},
+        {path:'contact', component:ContactPage},
+        {path:'', redirectTo:'home',pathMatch:'prefix'},
+        {path:'**', redirectTo:'home'}
+    ]},
+{path:'private', component:PrivateTemplate, children:[
+    {path:'dashboard', component:DashboardPage},
+    {path:'users', component:UsersPage},
+    {path:'', redirectTo:'dashboard',pathMatch:'prefix'},
+    {path:'**',redirectTo:'dashboard'}
+   
+]},
+{path:'',redirectTo:'public', pathMatch:'full'},
+{path:'**',redirectTo:'public'}
+];
