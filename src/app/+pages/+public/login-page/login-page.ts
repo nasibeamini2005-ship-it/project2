55bcbf582d8e35fa5258773f49b2e84f597bf6c3
Nasibeh Amini <nasibe.amini2005@gmail.com>
2025-11-23 05:46:40 +0330

    modified pages

--- src/app/+pages/+public/login-page/login-page.ts
+++ src/app/+pages/+public/login-page/login-page.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
-
+  username:string='';
+  password:string='';
+  remember:boolean=false;
+  massage:string='';
+  router=inject(Router); //  در اینجا به روتر انگولار نیاز داریم
+login(){
+if(this.username=='admin'&& this.password=='admin'){
+  this.router.navigateByUrl('/private/dashboard')
+}
+else{
+this.massage='نام کاربری یا کلمه عبور صحیح نیست!';
+}
+}
 }
